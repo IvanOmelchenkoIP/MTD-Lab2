e8d71774a49987e3a79352b25c15b43e2055e4c4
IvanOmelchenkoIP <io25112002@gmail.com>
2022-04-03 16:07:35 +0300

    Add program and test files
    Add files where the Lined List will be describes as well as files for
    unit testing Linked List class

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTD_Lab2;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MTD_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Circular Singly Linked List!");
+        }
+    }
+}
