66306087f500568ba69cca4fc7a69a82216771c3
IvanOmelchenkoIP <io25112002@gmail.com>
2022-04-03 16:22:10 +0300

    Add structure to CircularList, describe CharNode
    Add methods to CircularList class which will be implemented. Describe
    CharNode class which will save information about Linked List single element

--- src/CircularList.cs
+++ src/CircularList.cs
@@ -6,10 +6,73 @@ namespace MTD_Lab2
 {
     public class CharNode
     {
-
+        public CharNode next;
+        public char value;
     }
+
     public class CircularList
     {
+        public CharNode head = null;
+        private int length = 0;
 
+        public int Length() 
+        {
+            return 0;
+        }
+
+        public void Append(char element)
+        {
+
+        }
+
+        public void Insert(char element, int index)
+        { 
+
+        }
+
+        public char Delete(int index)
+        {
+            return '0';
+        }
+
+        public void DeleteAll(char element)
+        {
+
+        }
+
+        public char Get(int index)
+        {
+            return '0';
+        }
+
+        public CircularList Clone()
+        {
+            return null;
+        }
+
+        public void Reverse()
+        {
+
+        }
+
+        public int FindFirst(char element)
+        {
+            return -1;
+        }
+
+        public int FindLast(char element)
+        {
+            return -1;
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void Extend(CircularList list)
+        {
+
+        }
     }
 }
