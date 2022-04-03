fc5b6cce3988e72696313232c0ba8ff26d3776ed
IvanOmelchenkoIP <io25112002@gmail.com>
2022-04-03 16:10:50 +0300

    Add CircularList and CharNode classes
    Add CharNode class which will describe the nodes of the Linked List that
    uses Character type as values. Add CircularList class which will
    describe Singly Linked Circular List

---
+++ src/CircularList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTD_Lab2
+{
+    public class CharNode
+    {
+
+    }
+    public class CircularList
+    {
+
+    }
+}
